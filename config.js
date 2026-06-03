f2fd28ce34893113846d60e2ff60220cdf661f73
tomidare1234 <tomidare1234@gmail.com>
2026-06-03 14:18:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzCy9gWyiQ4R7fRRRv0grQBEg82GzTb3i6iV9tqUcz_MoMe_JN3aV9zxRsTpBV7rliu/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxQcm-wUxsVQekZz4Rp5yAO-b5eO7Ig6lC3Uz0jGhsSeVvEz8DsxlY9PJK_DHYKMZpU/exec";
